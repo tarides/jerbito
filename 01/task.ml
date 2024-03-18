a80cf2d08e081e94c74a6a8c9f97cb698a40af1e
SAMOURA AMADOU <47641181+samoura-amadou@users.noreply.github.com>
2024-03-18 11:27:24 +0000

    Update exercice 1 et 2

--- 01/task.ml
+++ 01/task.ml
@@ -8,4 +8,70 @@ if c < '\255' then
 let cr = "scream" in
 let i_u_we = "I scream, you scream, we all " ^ cr in
 let result = i_u_we ^ " for ice " in
+
 result;;
+
+
+
+let rec char_range lo hi =
+  if lo > hi then
+    ""
+  else
+    String.make 1 lo ^ char_range (char_succ lo) hi
+;;
+
+Devoir à faire : 
+
+1. Écrire une fonction qui prend en paramètre un entier n et qui
+(* Exemple d'utilisation *)
+char_range 'a' 'd';;
+(* Résultat: "abcd" *)
+
+
+let rec char_range_rev_cat s lo hi =
+  if lo > hi then
+    s
+  else
+    let prev_char = Char.chr (Char.code hi - 1) in
+    char_range_rev_cat (String.make 1 hi ^ s) lo prev_char
+;;
+
+Devoir à faire
+(* Exemple d'utilisation *)
+char_range_rev_cat "123" 'a' 'd';;
+(* Résultat: "dcba123" *)
+
+
+let rec char_range lo hi =
+  if lo > hi then
+    ""
+  else
+    String.make 1 lo ^ char_range (char_succ lo) hi
+;;
+Devoir à faire
+
+
+let rec char_range_rev_cat s lo hi =
+  if lo > hi then
+    s
+  else
+    char_range_rev_cat (String.make 1 lo ^ s) (Char.chr (Char.code lo + 1)) hi
+
+
+
+let char_range_rev lo hi =
+  let rec aux acc lo hi =
+    if lo > hi then
+      acc
+    else
+      aux (string_cons hi acc) lo (char_pred hi)
+  in
+  aux "" lo hi
+;;
+
+(* Exemple d'utilisation *)
+char_range_rev 'a' 'd';;
+(* Résultat: "dcba" *)
+;;
+
+
